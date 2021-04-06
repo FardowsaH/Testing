e0c62aea5d7bd0ddec0e5dbe15eb571ec8eb00f0
Fardowsa <fardowsa@yournextconcepts.com>
2021-04-06 21:45:00 +0200

    unit tests

--- src/main/java/domain/rules/RockPaperScissors.java
+++ src/main/java/domain/rules/RockPaperScissors.java
@@ -5,34 +5,33 @@ import domain.Result;
 import domain.Rules;
 
 public class RockPaperScissors implements Rules {
+	
     @Override
     public Result decide(Move mine, Move theirs) {
+    	
+        if (mine.equals(theirs)){
+             return Result.DRAW;
+         }
+    	
         switch (mine) {
             case ROCK:
-                switch (theirs) {
-                    case SCISSORS:
-                        return Result.WIN;
-                    case PAPER:
-                        return Result.LOSE;
-                }
+            	 return choose(theirs.equals(Move.SCISSORS));
+              
+            case SCISSORS:
+            	 return choose(theirs.equals(Move.PAPER));
 
             case PAPER:
-                switch (theirs) {
-                    case ROCK:
-                        return Result.WIN;
-                    case SCISSORS:
-                        return Result.LOSE;
+            	  return choose(theirs.equals(Move.ROCK));
                 }
-
-            case SCISSORS:
-                switch (theirs) {
-                    case PAPER:
-                        return Result.WIN;
-                    case ROCK:
-                        return Result.LOSE;
-                }
-        }
-
-        return Result.DRAW;
+		return null;
+            
+      
+    }
+    
+    public Result choose(boolean choose) {
+    	
+    	Result result = choose? Result.WIN:Result.LOSE;
+    	return result;
+       
     }
 }
